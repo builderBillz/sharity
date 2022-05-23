16885c4b6791fc144355ea5a1fe4437a7ee72f93
Melissa Lopez <melissalopez@pursuit.org>
2022-05-23 12:21:02 -0400

    added navbar links

---
+++ Front-End/src/Components/NavBar/NavBar.js
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+   <>
+   <nav className='NavBar'>
+      <ul>
+        <Link to='/'>Home</Link>
+      </ul>
+      <ul>
+        <Link to="/about">About Us</Link>
+        </ul>
+        <ul>
+        <Link to="/products">Products</Link>
+        </ul>
+        <ul>
+        <Link to="/account">My Account</Link>
+        </ul>
+    </nav>
+    </>
+  );
+};
+
+
+export default NavBar;
