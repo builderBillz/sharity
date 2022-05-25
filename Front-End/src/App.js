5e6a48b764bff4b93a2e51238d2fd63e94b628e8
Angie G <smiles3a@yahoo.com>
2022-05-25 11:46:25 -0400

    Add LogIn Route

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -6,11 +6,9 @@ import Home from "./Pages/Home";
 import Test from "./Pages/Test";
 import ProductDetails from "./Components/ProductDetails/ProductDetails";
 import Products from "./Components/Products/Products";
+import LogIn from "./Components/LogIn";
 
 // Components
-
-// const URL = process.env.REACT_APP_API_URL;
-// console.log(URL);
 
 function App() {
   return (
@@ -21,7 +19,8 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="/users" element={<Test />} />
         <Route path="/products/:id" element={<ProductDetails />} />
-        <Route path="/products" element={<Products />}/>
+        <Route path="/products" element={<Products />} />
+        <Route path="/login" element={<LogIn />} />
       </Routes>
     </div>
   );
