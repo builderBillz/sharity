ab84680bff86d797dc4a7c2026b6bd9fa1d48f30
Billy Dominique <billydominique@pursuit.org>
2022-05-31 18:56:29 -0400

    added links for individual products

--- Front-End/src/Components/Product/Product.js
+++ Front-End/src/Components/Product/Product.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Product({ product, addToCart }) {
 //passed addToBag as props from products components to update how many items in cart
 
@@ -5,8 +7,10 @@ function Product({ product, addToCart }) {
     <div className="container">
       <div className="productCard">
         <div>
-          <img src={product.image} alt={product.name} width="18%" />
-          <p>{product.name}</p>
+          <Link to={`/products/${product.id}`}>
+            <img src={product.image} alt={product.name} width="18%" />
+            <p>{product.name}</p>
+          </Link>
           <p>{product.description}</p>
           <p>{product.price}</p>
           <button onClick={() => addToCart(product)}>Add to Cart</button>
