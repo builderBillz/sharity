558280d009dbadbf3f5615c50b666dc7419d988b
Billy Dominique <billydominique@pursuit.org>
2022-06-04 17:29:59 -0400

    fixed rendering issue with shopping cart when there are no products in the shopping cart, adjusted subtotal and added tax and total to be display on the shopping cart component

--- Front-End/src/Components/ShoppingCart/ShoppingCart.js
+++ Front-End/src/Components/ShoppingCart/ShoppingCart.js
@@ -7,7 +7,7 @@ const ShoppingCart = (props) => {
 const HandleDelete = (event) => {
     props.func(event.target.id) 
 }
-    console.log(props.cart)
+    console.log(props)
 
     // const {
     //     isEmpty,
@@ -25,56 +25,82 @@ const HandleDelete = (event) => {
 
 //     </div>
 
-const prices = props.cart.map((product) => product.price)
-const total = prices.reduce((a,b) => a + b )
-console.log(total)
-
-return (
-    <div className='container-fluid py-5'>
-        <div className='row gy-4'>
-            <h1 className='text-center'>ShoppingCart</h1>
-            <div className='col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-8 py-4'>
-                <div className='d-flex justify-content-center py-3'>
-                    <div className='justify-content-center'>
-
-                        <table className='table table-primary-light table-hover m-0'>
-                            <tbody className='justify-content-center'>
-                                {props.cart.map((product, id) => {
-                                    return (
-                                        <tr className='align-middle' key={id} >
-                                        <td>
-                                            <Link to={`/products/${product.id}`}>
-                                                <img src={product.image} alt={product.name} width="55%" />
-                                            </Link>
-                                            </td>
-                                        <td className="card-title align-top fw-bolder">
-                                            <Link to={`/products/${product.id}`}>
-                                                {product.name}
-                                            </Link>
-                                            </td>
-                                        <td className="text-secondary">{product.description}</td>
-                                        <td className="align-top fw-bolder">${product.price}</td>
-                                        
-                                        <td>
-
-                                            <button onClick={HandleDelete} id={product.id} className="btn btn-outline-danger mx-1">Remove</button> 
-                                        </td>
-                                    </tr>
-
-    )
-    }
-    )}
-                            </tbody>
-                        </table>
+if (!props.cart.length) {
+    
+    return (
+        <div className='container-fluid py-5'>
+            <div className='row gy-4'>
+                <h1 className='text-center'>ShoppingCart</h1>
+                <div className='col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-8 py-4'>
+                    <div className='d-flex justify-content-center py-3'>
+                        <div className='justify-content-center'>                   
+                                                Your cart is empty 
+                                                <Link  to={`/products`}>
+                                                    Click Here
+                                                </Link>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div>
-            <p>Subtotal = ${total}</p>
+    )
+} else {
+
+    const prices = props.cart.map((product) => product.price)
+    const subtotal = (prices.reduce((a,b) => a + b ))
+    const tax = Number((subtotal * 0.08875).toFixed(2))
+    const total = Number((subtotal + tax).toFixed(2))
+    console.log(total)
+
+    return (
+        <div className='container-fluid py-5'>
+            <div className='row gy-4'>
+                <h1 className='text-center'>ShoppingCart</h1>
+                <div className='col-sm-12 col-md-12 col-lg-8 col-xl-8 col-xxl-8 py-4'>
+                    <div className='d-flex justify-content-center py-3'>
+                        <div className='justify-content-center'>
+    
+                            <table className='table table-primary-light table-hover m-0'>
+                                <tbody className='justify-content-center'>
+                                    {props.cart.map((product, id) => {
+                                        return (
+                                            <tr className='align-middle' key={id} >
+                                            <td>
+                                                <Link to={`/products/${product.id}`}>
+                                                    <img src={product.image} alt={product.name} width="55%" />
+                                                </Link>
+                                                </td>
+                                            <td className="card-title align-top fw-bolder">
+                                                <Link to={`/products/${product.id}`}>
+                                                    {product.name}
+                                                </Link>
+                                                </td>
+                                            <td className="text-secondary">{product.description}</td>
+                                            <td className="align-top fw-bolder">${product.price}</td>
+                                            
+                                            <td>
+    
+                                                <button onClick={HandleDelete} id={product.id} className="btn btn-outline-danger mx-1">Remove</button> 
+                                            </td>
+                                        </tr>
+    
+        )
+        }
+        )}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <p>Subtotal = ${subtotal}</p>
+                <p>Tax = ${tax}</p>
+                <p>Total = ${total}</p>
+            </div>
         </div>
-    </div>
-)
-};
+    )
+    };  
+} 
 
 export default ShoppingCart;
