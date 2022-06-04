3f70c2660e48575c8b8991958eed1915b338fb9d
Billy Dominique <billydominique@pursuit.org>
2022-06-04 08:49:28 -0400

    restored product links in product component after merge changes

--- Front-End/src/Components/Product/Product.js
+++ Front-End/src/Components/Product/Product.js
@@ -1,15 +1,18 @@
 
 import { Button, Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Product({ product, addToCart }) {
 //passed addToBag as props from products components to update how many items in cart
-
   return (
       <Card className="h-100 shadow rounded" style={{ color: "#000"}}>
+          <Link to={`/products/${product.id}`}>
           <Card.Img src={product.image} alt={product.name} className="card-img-top img-fluid" />
+          </Link>
           <Card.Body className="d-flex flex-column">
-
+            <Link to={`/products/${product.id}`}>
             <Card.Title>{product.name}</Card.Title>
+            </Link>
             <Card.Text className="text-secondary">{product.description}</Card.Text>
             <Card.Text className="card text-end fw-bolder">${product.price}</Card.Text>
             <div className="d-grid justify-content-end mt-4">
